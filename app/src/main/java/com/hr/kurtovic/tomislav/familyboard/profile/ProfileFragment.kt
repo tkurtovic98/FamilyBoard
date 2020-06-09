61b9a3d48523e1478d2d7128d96f8fd733d50a8f
tkurtovic <tkurtovic98@gmail.com>
2020-06-10 01:13:10 +0200

    feat(profile): add progresBar on loading

--- app/src/main/java/com/hr/kurtovic/tomislav/familyboard/profile/ProfileFragment.kt
+++ app/src/main/java/com/hr/kurtovic/tomislav/familyboard/profile/ProfileFragment.kt
@@ -44,18 +44,24 @@ class ProfileFragment : Fragment() {
     }
 
     private fun render(state: State) {
+        profile_progress_bar.visibility = if (state.loading) {
+            View.VISIBLE
+            return
+        } else View.GONE
 
         if (state.spinnerConfigure) {
             configureSpinner(state.familyList)
             profileViewModel.onEvent(Event.SpinnerConfigured)
         }
 
-        if (state.profileImageLoad) {
+        if (state.firstTimeLoading) {
             state.currentMember?.apply {
                 Glide.with(requireContext())
                         .load(this.urlPicture)
                         .circleCrop()
                         .into(profile_profile_image)
+
+                profile_member_name.text = this.name
             }
             profileViewModel.onEvent(Event.FirstTimeLoaded)
         }
